b7865632c3b52552d2a6b7b80b34ce10a1bda116
Anton029040 <161502926+Anton029040@users.noreply.github.com>
2025-04-10 22:56:59 +0200

    Satt upp Maven samt gjort första utkast på GUI

---
+++ src/main/java/Controller/Launcher.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import View.Login.LoginController;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(LoginController.class, args);
+    }
+}
